1636b21c9fa86234cec93e770366896feb27c8fa
Ricardo Maya <rickmaya@gmail.com>
2021-11-02 12:45:00 -0500

    modify: use the taskToComplet variable on the updateOne method filter property.  remove the task id from the ejs file. fix urls in ejs file from todo to todos

--- src/controllers/todoControllers.js
+++ src/controllers/todoControllers.js
@@ -1,13 +1,10 @@
 const Todo = require('../models/todoSchema');
-const {render} = require("ejs");
 
 // Fetches all tasks
 exports.fetchTasks = async (req, res) => {
   try {
     const tasks = await Todo.find();
-    if (!tasks) {
-      console.log('There are no tasks to complete.');
-    }
+
     res.render('index.ejs', {todos: tasks});
   } catch (err) {
     return res.json({message: err});
@@ -27,7 +24,6 @@ exports.createNewTask = async (req, res) => {
     }
 
     await newTodo.save()
-    console.log(`This is the newly saved task ===> ${newTodo}`);
     res.redirect('/todos');
   } catch (err) {
     console.log(err);
@@ -37,11 +33,7 @@ exports.createNewTask = async (req, res) => {
 // Delete a task
 exports.deleteTask = async (req, res) => {
   try{
-    // const id = req.params.id;
-    // console.log(`This is the id of the task selected to be deleted ===> ${id}`);
-
     await Todo.deleteOne({_id: req.params.id});
-
 
     res.status(200).redirect('/todos');
   } catch (err) {
@@ -54,10 +46,6 @@ exports.findTaskToUpdate = async (req, res) => {
   try {
     let taskToUpdate = await Todo.findOne({_id: req.params.id});
     console.log(`Here is the taskToUpdate ====> ${taskToUpdate}`);
-
-    if (!taskToUpdate) {
-      return res.status(404).json({message: "todo not found"});
-    }
 
     res.status(200).render('edit.ejs', {todo: taskToUpdate});
   } catch (err) {
@@ -77,7 +65,7 @@ exports.updateTask = async (req, res) => {
       {new: true}
     );
 
-    res.redirect('/todos');
+    res.status(200).redirect('/todos');
   } catch (err) {
     console.log(err);
   }
@@ -87,20 +75,15 @@ exports.updateTask = async (req, res) => {
 exports.completeTask = async (req, res) => {
   try {
     let taskToComplete = await Todo.findById(req.params.id);
-    console.log(`This is the task to set as complete ===> ${taskToComplete}`);
 
     let completedTask = await Todo.updateOne(
-      { id: req.params.id },
+      taskToComplete,
       { done: true },
       {new: true}
     );
-    console.log(`This is the task marked for completion ===> ${JSON.stringify(completedTask)}`);
 
-    // get all the todos
-    const todos = Todo.find();
-
-    return render('index.ejs', {todos: todos});
-  } catch (e) {
-    console.log(e);
+    res.status(200).redirect('/todos');
+  } catch (err) {
+    console.log(err);
   }
 }
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -8,16 +8,16 @@ router.get('/todos', TodoController.fetchTasks);
 router.post('/todos', TodoController.createNewTask);
 
 // GET route to delete a task
-router.get('/todo/:id/delete', TodoController.deleteTask);
+router.get('/todos/:id/delete', TodoController.deleteTask);
 
 // GET route to find task to edit
-router.get('/todo/:id/edit', TodoController.findTaskToUpdate);
+router.get('/todos/:id/edit', TodoController.findTaskToUpdate);
 
 // POST route to update task
 router.post('/todos/:id', TodoController.updateTask);
 
 // GET route to mark task as done
-router.get('/todo/:id/completed', TodoController.completeTask);
+router.get('/todos/:id/completed', TodoController.completeTask);
 
 
 
